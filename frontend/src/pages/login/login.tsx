c964ccc11b31b4581c34d1d35cffc4f9776381b5
Ray Cai Flanagan <hjkareus@gmail.com>
2025-02-02 12:04:29 -0500

    Update login.tsx

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -84,13 +84,6 @@ export default function Login() {
         <div className="min-h-screen bg-[#f3f1ea] content-center">
             <main className="flex-1 flex flex-col items-center">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Compaist Logo"
-                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto cursor-pointer"
-                        onClick={() => navigate('/')}
-                    />
-
                     <h2
                         className="mt-10 text-center text-3xl/9 font-bold tracking-tight text-gray-900"
                         style={{ fontFamily: 'var(--font-family)' }}
